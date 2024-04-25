c949e3310859bf951bc0a0bbbd7f106b47a6f6e3
lihqi <455711093@qq.com>
2024-04-25 14:45:37 +0800

    fix(lb-components): Add types

--- packages/lb-components/src/components/pointCloud2DRectOperationView/index.tsx
+++ packages/lb-components/src/components/pointCloud2DRectOperationView/index.tsx
@@ -9,7 +9,7 @@ import { a2MapStateToProps } from '@/store/annotation/map';
 import { LabelBeeContext } from '@/store/ctx';
 import { IMappingImg } from '@/types/data';
 import { ImgUtils, PointCloud2DRectOperation } from '@labelbee/lb-annotation';
-import { IBasicRect } from '@labelbee/lb-utils';
+import { IBasicRect, IPointCloudBoxRect } from '@labelbee/lb-utils';
 
 import { TAfterImgOnLoad } from '../AnnotationView';
 import _ from 'lodash';
@@ -30,6 +30,7 @@ interface IPointCloud2DRectOperationViewRect extends IBasicRect {
   id: string;
   attribute: any;
   order?: number;
+  imageName?: string;
 }
 
 const PointCloud2DRectOperationView = (props: IPointCloud2DRectOperationViewProps) => {
@@ -53,8 +54,10 @@ const PointCloud2DRectOperationView = (props: IPointCloud2DRectOperationViewProp
 
   const [loading, setLoading] = useState(true);
 
-  const rectListInImage = rectList?.filter((item) => item.imageName === mappingData?.path);
-  const mappingDataPath = useLatest<any>(mappingData?.path);
+  const rectListInImage = rectList?.filter(
+    (item: IPointCloudBoxRect) => item.imageName === mappingData?.path,
+  );
+  const mappingDataPath = useLatest(mappingData?.path);
 
   const handleUpdateDragResult = (rect: IPointCloud2DRectOperationViewRect) => {
     const { boxID } = rect;
